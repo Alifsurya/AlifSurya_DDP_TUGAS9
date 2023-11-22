e6b727ac88bc77a466ecede8ddb561f5bd247bd0
Alifsurya <146050995+Alifsurya@users.noreply.github.com>
2023-11-22 15:12:26 +0700

    Add files via upload

---
+++ Tugas9.py
@@ -0,0 +1,9 @@
+#latihan
+def profile(nama, alamat, gender, umur, hobi):
+    print("Nama saya adalah : ", nama)
+    print("Tempat tinggal saya di : ", alamat)
+    print("saya berjenis kelamin : ", gender)
+    print("saya berusia : ", umur)
+    print("Hobi saya : ", hobi)
+
+profile("Alif", "Jakarta", "Laki-laki", "20", "Bermain Game")
---
+++ Tugas92.py
@@ -0,0 +1,5 @@
+def ganjil(bilangan):
+    for i in range(bilangan):
+        if i % 2 != 0:
+            print(i)
+ganjil(100)
